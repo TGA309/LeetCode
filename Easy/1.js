dd4f6c4cefa61bac5ab19ffb6df43d5c87d10721
BuildTools <unconfigured@null.spigotmc.org>
2023-01-06 07:02:13 +0800

    Solved the First Question.

---
+++ Easy/1.js
@@ -0,0 +1,21 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+
+var twoSum = function(nums, target) {
+
+    for(let i = 0; i <= nums.length - 1; i++) {
+
+        for(let j = 0; j <= nums.length - 1; j++) {
+
+            if(nums[i] + nums[j] == target && i != j) {
+
+                number = [i, j];
+                return number;
+            }
+        }
+    }
+
+};
