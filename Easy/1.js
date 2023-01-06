6f1dca83528853b80974b73b8001c28ec83200e7
BuildTools <unconfigured@null.spigotmc.org>
2023-01-06 22:52:03 +0800

    Improved the second loop by eliminating previously checked indices.

--- Easy/1.js
+++ Easy/1.js
@@ -8,9 +8,9 @@ var twoSum = function(nums, target) {
 
     for(let i = 0; i <= nums.length - 1; i++) {
 
-        for(let j = 0; j <= nums.length - 1; j++) {
+        for(let j = i + 1; j <= nums.length - 1; j++) {
 
-            if(nums[i] + nums[j] == target && i != j) {
+            if(nums[i] + nums[j] == target) {
 
                 number = [i, j];
                 return number;
